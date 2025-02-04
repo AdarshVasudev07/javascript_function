5e5e828ba419d483f412a82af790b6c7ca498072
AdarshVasudev07 <adarshvasudev8055@gmail.com>
2025-02-04 13:56:07 +0530

    jquery bascics

---
+++ javascript/index.js
@@ -0,0 +1,48 @@
+// array form()
+
+// const str = "adarsh"
+// const arr = Array.from(str);
+
+// console.log(arr);
+
+
+// const person = {
+//     name:"adarsh",
+//     age  :"21",
+//     profession:"developer"
+
+
+
+// }
+// console.table(person);
+
+
+// empty an Array
+
+// let numbers = [1,2,3,4,5]
+
+// numbers.length = 1;
+// console.log(numbers);
+
+
+// map ()function  (the map() function to transform elements in a new array without modifying the orginal array)
+
+
+// const numbers =[1,2,3,4,5];
+// const doubled = numbers.map(num => num*3);
+// console.log(numbers);
+// console.log(doubled);
+
+
+let numbers = [1,6,4,3,8,15]
+let evens = numbers.filter(num => num % 2 ===0);
+console.log(numbers);
+console.log(evens);
+
+
+
+
+
+
+
+
